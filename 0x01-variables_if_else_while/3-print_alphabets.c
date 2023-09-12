e278dcbff71980122dc780c4cfd55140d31a0bf0
YacineAE <ouadadyassine95@gmail.com>
2023-09-12 09:01:54 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char lowercase='a';
+	char Uppercase='A';
+
+	while(lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while(Uppercase <= 'Z')
+	{
+		putchar(Uppercase);
+		Uppercase++;
+	}
+	putchar('\n');
+	return (0);
+}
