4cb62b36767582e9e1abef2c12b10a6b94cfe114
YacineAE <ouadadyassine95@gmail.com>
2023-09-13 13:47:59 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,16 @@
  * Return: Always 0 (Success)
  */
 
-int _putchar(char ch[])
+int main(void)
 {
-	char ch[] = "/_putchar";
-
-	for (i = 0; ch[i] != '\0'; i++)
-	{
-		_putchar(c[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return 0;
 }
