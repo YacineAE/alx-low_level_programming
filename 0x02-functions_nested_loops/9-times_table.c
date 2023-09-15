dd22eed12cfa515c0d577a04a37e0dad30db8ef8
YacineAE <ouadadyassine95@gmail.com>
2023-09-15 13:27:18 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,12 +16,12 @@ void times_table(void)
                         {
                                 _putchar(p + '0');
                         }
-                        if ((k < 10) && (j != 0))
+                        if ((p < 10) && (j != 0))
                         {
                                 _putchar(',');
                                 _putchar(' ');
                                 _putchar(' ');
-                                _putchar(k + '0');
+                                _putchar(p + '0');
                         }
                         else if (p >= 10)
                         {
