da04378aefbe33545c6d7678ba5cb3a88f7f24c4
YacineAE <ouadadyassine95@gmail.com>
2023-09-15 15:43:05 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,54 +1,31 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_to_98 - prints all natural numbers from n to 98
- *
- * Return: Always 0.
  */
 void print_to_98(int n)
 {
-        int i, j;
+	int i, j;
 
-        if (n <= 98)
-        {
-                for (i = n; i <= 98; i++)
-                {
-                        if (i >= 10)
-                        {
-                                _putchar((i / 10) + '0');
-                                _putchar((i % 10) + '0');
-                        }
-                        else
-                        {
-                                _putchar(n + '0');
-                        }
-                        if (i < 98)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                }
-        }
-        else
-        {
-                for (j = n; j >= 98; j--)
-                {
-                        if (j >= 10)
-                        {
-                                _putchar((j / 10) + '0');
-                                _putchar((j % 10) + '0');
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-                        else
-                        {
-                                _putchar(n + '0');
-                        }
-                        if (j > 98)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                }
-        }
-        _putchar('\n');
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
 }
 
