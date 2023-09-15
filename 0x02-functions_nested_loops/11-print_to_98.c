fef9296c433467e7477f8041f06910abb8f70bf3
YacineAE <ouadadyassine95@gmail.com>
2023-09-15 15:42:01 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,47 +7,48 @@
  */
 void print_to_98(int n)
 {
-	int i;
+        int i, j;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				_putchar(n + '0');
-			}
-			if (i < 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+        if (n <= 98)
+        {
+                for (i = n; i <= 98; i++)
+                {
+                        if (i >= 10)
+                        {
+                                _putchar((i / 10) + '0');
+                                _putchar((i % 10) + '0');
+                        }
+                        else
+                        {
+                                _putchar(n + '0');
+                        }
+                        if (i < 98)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                }
+        }
+        else
+        {
+                for (j = n; j >= 98; j--)
+                {
+                        if (j >= 10)
+                        {
+                                _putchar((j / 10) + '0');
+                                _putchar((j % 10) + '0');
 		}
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				_putchar(n + '0');
-			}
-			if (i > 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	_putchar('\n');
+                        else
+                        {
+                                _putchar(n + '0');
+                        }
+                        if (j > 98)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                }
+        }
+        _putchar('\n');
 }
+
