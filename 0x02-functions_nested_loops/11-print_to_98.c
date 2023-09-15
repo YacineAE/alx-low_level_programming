d78b418dd9fa6aa0e9fcc60fac2a0cbf27619d08
YacineAE <ouadadyassine95@gmail.com>
2023-09-15 15:08:59 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,32 +6,47 @@
  */
 void print_to_98(int n)
 {
+	int i;
 
-	if (n < 98)
+	if (n <= 98)
 	{
-		while (n < 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(n +'0');
-			_putchar(',');
-			_putchar(' ');
-			n++;
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+			if (i < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		_putchar('\n');
 	}
-	else if (n == 98)
+	else
 	{
-		_putchar(n + '0');
-		_putchar('\n');
-	}
-	else if (n > 98)
-	{
-		while (n > 98)
+		for (i = n; i >= 98; i--)
 		{
-			_putchar(n +'0');
-			_putchar(',');
-			_putchar(' ');
-			n--;
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+			if (i > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
