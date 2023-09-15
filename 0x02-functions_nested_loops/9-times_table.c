0bbdcbcb1ccf958e4efc29cd007cf5154e32b3a2
YacineAE <ouadadyassine95@gmail.com>
2023-09-15 10:19:47 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,24 +14,26 @@ void times_table(void)
 			p = i * j;
 			if (j == 0)
 			{
-				_putchar(' ');
 				_putchar('0');
 			}
-			if ((p < 10) && (j != 0))
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(p + '0');
 			}
-			else if ((p >=10) && (j != 0))
+			if (p < 10)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar('0');
+			}
+			else
+			{
 				_putchar((p / 10) + '0');
 				_putchar((p % 10) + '0');
 			}
+			if (j == 9)
+			{
+				_putchar('\n');
+			}
 		}
-		_putchar('\n');
 	}
 }
