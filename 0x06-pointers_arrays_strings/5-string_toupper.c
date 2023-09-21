c515326dbbde66a6b7a96d70c33df256e06a77cb
YacineAE <ouadadyassine95@gmail.com>
2023-09-21 15:19:44 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,12 +6,12 @@
  * Return: str
  */
 char *string_toupper(char *str)
-{       
-        int i = 0;
-        
-        while (str[i] != '\0')
-        {       
-                if (str[i] >= 'a' && str[i] <= 'z')
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{       
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
