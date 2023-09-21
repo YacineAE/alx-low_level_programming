3bfecb6415c710594fe749f1d3000afe627b2e2a
YacineAE <ouadadyassine95@gmail.com>
2023-09-21 16:17:48 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,39 @@
 #include "main.h"
+
 /**
- * cap_string - capitalize all words of a string
- * @str: the input string
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
  *
- * Return: str
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
-	int i;
-	if (str[i] >= 'a' && str[i] <= 'z')
+	int index = 0;
+
+	while (str[index])
 	{
-		str[0] = str[0] - 32;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	for (i = 1; str[i] != '\0'; i++)
-	{
-		if ((str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') && (str[i + 1] >= 'a' && str[i +1] <= 'z'))
-		{
-			str[i + 1] = str[i + 1] - 32;
-		}
-	}
+
 	return (str);
 }
+
