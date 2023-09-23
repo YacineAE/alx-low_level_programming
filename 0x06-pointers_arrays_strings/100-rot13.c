722a30e7139773b0fc7f591ab809762439332d02
YacineAE <ouadadyassine95@gmail.com>
2023-09-23 15:06:19 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encode a string using rot13
+ * @str: the input string to be encoded
+ *
+ * Return: a pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			str[i] = str[i] + 13;
+		}
+		if ((str[i] > 'z' && str[i] <= ('z' + 13)) || (str[i] > 'Z' && str[i] <= ('Z' + 13)))
+		{
+			str[i] = str[i] - 26;
+		}
+		i++;
+	}
+	return (str);
+}
