ef5e16ccd54f35e9517c9f384ed247803b351ead
YacineAE <ouadadyassine95@gmail.com>
2023-09-29 14:42:15 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,10 +6,10 @@
  * Return: factorial of the given number
  */
 int factorial(int n)
-{       
-        if (n == 0 || n == 1)
-        {
-                return (1);
+{
+	if (n == 0 || n == 1)
+	{
+		return (1);
 	}
 	else if (n < 0)
 	{
